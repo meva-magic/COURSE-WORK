40590f126bdd84e436e20e4e74f5173275ecf9a3
saharov-dev <saharov.dev@gmail.com>
2023-12-03 15:23:15 +0300

    ;0

--- COURSE WORK/Assets/Scripts/Sound/SceneMusic.cs
+++ COURSE WORK/Assets/Scripts/Sound/SceneMusic.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class SceneMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        AudioManager.instance.Play("")
-        
+        //AudioManager.instance.Play("")
     }
-
-
+}
